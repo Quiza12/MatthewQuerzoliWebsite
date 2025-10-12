bc895196d9151f23a3d19d428cb650554c97e415
Quiza12 <Quiza12@live.com>
2025-10-13 08:31:27 +1100

    Updates to Quiet + MVP for INTERNET RECIPE.

--- src/app/projects/internet-recipe/internet-recipe.component.ts
+++ src/app/projects/internet-recipe/internet-recipe.component.ts
@@ -10,10 +10,55 @@ import { InfiniteScrollModule } from 'ngx-infinite-scroll';
   styleUrl: './internet-recipe.component.css'
 })
 export class InternetRecipeComponent {
-  items = Array.from({ length: 492 }, (_, i) => `Item #${i + 1}`);
 
-  onScroll(): void {
-    const nextItems = Array.from({ length: 20 }, (_, i) => `Item #${this.items.length + i + 1}`);
-    this.items = [...this.items, ...nextItems];
+  array: any[] = [];
+  sum = 100;
+  throttle = 300;
+  scrollDistance = 1;
+  scrollUpDistance = 1;
+  direction = "";
+
+  constructor() {
+    this.appendItems(0, this.sum);
   }
+
+  addItems(startIndex: any, endIndex: any, _method: any) {
+    for (let i = 0; i < this.sum; ++i) {
+      // this.array.push("Hello")
+      this.array[_method]([i, " ", this.generateWord()].join(""));
+    }
+  }
+
+  appendItems(startIndex: any, endIndex: any) {
+    this.addItems(startIndex, endIndex, "push");
+  }
+
+  prependItems(startIndex: any, endIndex: any) {
+    this.addItems(startIndex, endIndex, "unshift");
+  }
+
+  onScrollDown() {
+    console.log("scrolled down");
+
+    // add another 20 items
+    const start = this.sum;
+    this.sum += 20;
+    this.appendItems(start, this.sum);
+
+    this.direction = "down";
+  }
+
+  onUp() {
+    console.log("scrolled up");
+    const start = this.sum;
+    this.sum += 20;
+    this.prependItems(start, this.sum);
+
+    this.direction = "up";
+  }
+  generateWord() {
+    // return chance.word();
+    return "word" + Math.floor((Math.random() * 100) + 1);
+  }
+
 }
