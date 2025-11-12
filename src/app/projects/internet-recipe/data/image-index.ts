d409451016826394e6a70e0b73c90f95ef47929b
Quiza12 <Quiza12@live.com>
2025-11-12 21:51:21 +1100

    INTERNET RECIPE more images and updates to navbar

--- src/app/projects/internet-recipe/data/image-index.ts
+++ src/app/projects/internet-recipe/data/image-index.ts
@@ -5,5 +5,10 @@ export const imageIndex: string[] = [
   '/assets/images/internet-recipe/food4.jpg',
   '/assets/images/internet-recipe/food5.jpg',
   '/assets/images/internet-recipe/food6.jpg',
+  '/assets/images/internet-recipe/food7.jpg',
+  '/assets/images/internet-recipe/food8.jpg',
+  '/assets/images/internet-recipe/food9.jpg',
+  '/assets/images/internet-recipe/food10.jpg',
+  '/assets/images/internet-recipe/food11.jpg'
 ];
 
