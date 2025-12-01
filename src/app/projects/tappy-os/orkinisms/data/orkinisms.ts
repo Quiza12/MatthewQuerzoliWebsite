8d17343127b886c3a568e268ac7b071899603cb7
Quiza12 <Quiza12@live.com>
2025-12-01 18:33:46 +1100

    More Orkinisms.

--- src/app/projects/tappy-os/orkinisms/data/orkinisms.ts
+++ src/app/projects/tappy-os/orkinisms/data/orkinisms.ts
@@ -3,16 +3,10 @@ import { Orkinism } from '../classes/orkinism';
 //word, type, phonetic, definition
 export const orkinisms: Orkinism[] = [
     new Orkinism(
-        'tookie',
+        'tookie; wookie',
         'noun',
         '[tuuK-kie]',
         'The family dog/prince/icon, Tyson Orkin, or Taryn\'s boyfriend, Matt.'
-    ),
-    new Orkinism(
-        'wookie',
-        'noun',
-        '[wuuk-Kee]',
-        'Also the family dog/prince/icon, Tyson Orkin, or Taryn\'s boyfriend, Matt.'
     ),
     new Orkinism(
         'noonkie',
@@ -24,7 +18,7 @@ export const orkinisms: Orkinism[] = [
         'twinkie',
         'noun',
         '[twin-KIE]',
-        'A vagina.'
+        'A twinkie.'
     ),
     new Orkinism(
         'Master Jack',
@@ -33,46 +27,58 @@ export const orkinisms: Orkinism[] = [
         'Gran\'s boyfriend name placeholder.'
     ),
     new Orkinism(
-        'tookalook',
+        'tookalook; tookalooks',
         'noun',
         '[took-A-look]',
         'How Chloe refers to one of the clan (can be any of the siblings).'
     ),
     new Orkinism(
-        'shinkalink',
+        'shink; shinkalink; shinkadink',
         'noun',
         '[]',
-        ''
+        'Shannon Orkin.'
+    ),
+    new Orkinism(
+        'taps; tips; tapsy; tipson',
+        'noun',
+        '[]',
+        'Taryn Orkin.'
+    ),
+    new Orkinism(
+        'shamin',
+        'noun',
+        '[]',
+        'When Hilton forgets all of the kids names and needs to call them something.'
+    ),
+    new Orkinism(
+        'hamin',
+        'noun',
+        '[]',
+        'When Hilton really gets confused (refer shamin).'
     ),
     new Orkinism(
         'doodle',
         'noun',
         '[]',
-        ''
+        'If someone is being silly; a poop (refer niggie).'
     ),
     new Orkinism(
         'niggie',
         'noun',
         '[]',
-        ''
+        'When the pug poops, the last person to yell this out has to pick up the poo.'
     ),
     new Orkinism(
         'tooksie',
         'noun',
         '[]',
-        'Used interghangeably with \'tookie\''
+        'Used interchangeably with \'tookie\''
     ),
     new Orkinism(
-        'puggles',
+        'puggles; puglet; the pug; pugloot; little face; handsome',
         'noun',
         '[]',
         'The pug.'
-    ),
-    new Orkinism(
-        '',
-        'noun',
-        '[]',
-        ''
     ),
     new Orkinism(
         'rookie',
@@ -92,10 +98,137 @@ export const orkinisms: Orkinism[] = [
         '[]',
         'Hilton\'s standard emoji response, for the defensive.'
     ),
+    new Orkinism(
+        'hoolamachala',
+        'noun',
+        '[?]',
+        'WAITING ON DEFINITION - Get on your bicycles?'
+    ),
+    new Orkinism(
+        'kom eetem de spraken',
+        'phrase',
+        '[?]',
+        'Come eat!'
+    ),
+    new Orkinism(
+        'padoonkie',
+        'phrase',
+        '[?]',
+        'A poop.'
+    ),
+    new Orkinism(
+        '...chicken scratch...',
+        'noun',
+        '[]',
+        'Undefinable, more of a noise.'
+    ),
+    new Orkinism(
+        'YOU {INSERT WORD/PHRASE}',
+        'noun',
+        '[]',
+        'Used to fire something back at someone (example: "Are you ready?" No, YOU ready!)'
+    ),
+    new Orkinism(
+        'doodoo; kip',
+        'noun',
+        '[]',
+        'A afternoon nap.'
+    ),
+    new Orkinism(
+        'dingleberry',
+        'noun',
+        '[]',
+        'When Tysie has poo stuck to his bum; '
+    ),
+    new Orkinism(
+        'fuck-knuckle',
+        'noun',
+        '[]',
+        'Chloe\'s peak level of affection for Hilton.'
+    ),
+    new Orkinism(
+        'dingbat',
+        'noun',
+        '[]',
+        'A detestable term for Hilton.'
+    ),
+    new Orkinism(
+        'milton; biltong; eaton; pigeon; guilton; elton; hamilton; elton; ',
+        'noun',
+        '[]',
+        'Hilton Orkin\'s names (at the altar, various coffee shops, etc).'
+    ),
+    new Orkinism(
+        'a double U',
+        'noun',
+        '[]',
+        'When you can\'t say the the work \'walk\' in front of Tysie.'
+    ),
+    new Orkinism(
+        'dramatic pug; drama llama',
+        'noun',
+        '[]',
+        'When someone is being dramatic.'
+    ),
+    new Orkinism(
+        'squish',
+        'noun',
+        '[]',
+        'When someone is being cute, perhaps overly so.'
+    ),
+    new Orkinism(
+        'NOIIIICCEEE',
+        'noun',
+        '[]',
+        'Historically, appreciation of a Christmas present.'
+    ),
+    new Orkinism(
+        'swipe swipe, swipe swipe',
+        'noun',
+        '[]',
+        'Appreciation of a Christmas present; Hilton\'s contribution to said present.'
+    ),
+    new Orkinism(
+        'See you on the Christmas tree',
+        'noun',
+        '[]',
+        'Gran - '
+    ),
+    new Orkinism(
+        'funnyface',
+        'noun',
+        '[]',
+        'Gran - '
+    ),
+    new Orkinism(
+        'fatface',
+        'noun',
+        '[]',
+        'Gran - '
+    ),
+    new Orkinism(
+        'weenie',
+        'noun',
+        '[]',
+        'Scaredy-cat (do not say to Chloe Orkin).'
+    ),
+    new Orkinism(
+        'dookie',
+        'noun',
+        '[]',
+        'Anything that sticks out a little; the little bump of the back of Ryan\'s head.'
+    ),
+    new Orkinism(
+        'longnecks',
+        'noun',
+        '[]',
+        'Giraffes (not to be confused with baby giraffes).'
+    ),
 ];
 
 
-// See you on the Christmas tree
+
+// 
 // Shinkies
 // Shinkalink
 // Shinks
