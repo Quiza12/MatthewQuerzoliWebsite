163010ce7f5ac3b14da13351a8062b58f03479ba
Quiza12 <Quiza12@live.com>
2025-12-16 19:17:34 +1100

    Adding phonetic info, search on keydown.

--- src/app/projects/tappy-os/orkinisms/data/orkinisms.ts
+++ src/app/projects/tappy-os/orkinisms/data/orkinisms.ts
@@ -11,7 +11,7 @@ export const orkinisms: Orkinism[] = [
     new Orkinism(
         'shink; shinkies; shinkalink; shinkadink; shinkalinky',
         'noun',
-        '[]',
+        '[shingk]',
         'Shannon Orkin.'
     ),
     new Orkinism(
@@ -47,236 +47,236 @@ export const orkinisms: Orkinism[] = [
     new Orkinism(
         'shamin',
         'noun',
-        '[]',
+        '[sham-in]',
         'When Hilton forgets all of the kids names and needs to call them something.'
     ),
     new Orkinism(
         'hamin',
         'noun',
-        '[]',
+        '[ham-in]',
         'When Hilton really gets confused (refer shamin).'
     ),
     new Orkinism(
         'doodle',
         'noun',
-        '[]',
+        '[dood-l]',
         'If someone is being silly; a poop (refer niggie).'
     ),
     new Orkinism(
         'niggie',
         'noun',
-        '[]',
+        '[nig-gee]',
         'When the pug poops, the last person to yell this out has to pick up the poo.'
     ),
     new Orkinism(
         'tooksie',
         'noun',
-        '[]',
+        '[took-see]',
         'Used interchangeably with \'tookie\''
     ),
 
     new Orkinism(
         'rookie',
         'noun',
-        '[]',
+        '[rook-ee]',
         'A misspelling of tookie/wookie. Also a televsion show.'
     ),
     new Orkinism(
         '👍🏼',
         'noun',
-        '[]',
+        '[N/A]',
         'Hilton\'s standard emoji response, for the affirmative.'
     ),
     new Orkinism(
         '🖕🏼',
         'noun',
-        '[]',
+        '[N/A]',
         'Hilton\'s standard emoji response, for the defensive.'
     ),
     new Orkinism(
         'hoolamachala',
         'noun',
-        '[?]',
-        'WAITING ON DEFINITION - Get on your bicycles?'
+        '[hoo-la-mach-uh-lah]',
+        'Hurry up - get on your bicycles!'
     ),
     new Orkinism(
         'kom eetem de spraken',
         'phrase',
-        '[?]',
+        '[N/A]',
         'Come eat!'
     ),
     new Orkinism(
         'padoonkie',
         'phrase',
-        '[?]',
-        'A poop.'
+        '[pad-oon-ee]',
+        'A poop, typically human.'
     ),
     new Orkinism(
         '...chicken scratch...',
         'noun',
-        '[]',
+        '[N/A]',
         'Undefinable, more of a noise.'
     ),
     new Orkinism(
         'YOU {INSERT WORD/PHRASE}',
         'noun',
-        '[]',
+        '[N/A]',
         'Used to fire something back at someone (example: "Are you ready?" No, YOU ready!)'
     ),
     new Orkinism(
         'doodoo; kip',
         'noun',
-        '[]',
+        '[doo-doo]',
         'A afternoon nap.'
     ),
     new Orkinism(
         'dingleberry',
         'noun',
-        '[]',
+        '[ding-guhl-ber-ee]',
         'When Tysie has poo stuck to his bum; '
     ),
     new Orkinism(
         'fuck-knuckle',
         'noun',
-        '[]',
+        '[fuhk-nuhk-uhl]',
         'Chloe\'s peak level of affection for Hilton.'
     ),
     new Orkinism(
         'dingbat',
         'noun',
-        '[]',
+        '[ding-bat]',
         'A detestable term for Hilton.'
     ),
     new Orkinism(
         'milton; biltong; eaton; pigeon; guilton; elton; hamilton; elton; clinton',
         'noun',
-        '[]',
-        'Hilton Orkin\'s names (at the altar, various coffee shops, etc).'
+        '[N/A]',
+        'Hilton Orkin\'s various names (at the altar, coffee shops, etc).'
     ),
     new Orkinism(
-        'a double U',
+        'W',
         'noun',
-        '[]',
+        '[duhb-uhl-yoo]',
         'When you can\'t say the the work \'walk\' in front of Tysie.'
     ),
     new Orkinism(
         'dramatic pug; drama llama',
         'noun',
-        '[]',
+        '[druh-mat-ik puhg]',
         'When someone is being dramatic.'
     ),
     new Orkinism(
         'squish',
         'noun',
-        '[]',
+        '[skwish]',
         'When someone is being cute, perhaps overly so.'
     ),
     new Orkinism(
         'NOIIIICCEEE',
         'noun',
-        '[]',
+        '[nooooois]',
         'Historically, appreciation of a Christmas present.'
     ),
     new Orkinism(
         'swipe swipe, swipe swipe',
         'noun',
-        '[]',
+        '[swahyp swahyp, swahyp swahyp]',
         'Appreciation of a Christmas present; Hilton\'s contribution to said present.'
     ),
     new Orkinism(
         'See you on the Christmas tree',
-        'noun',
+        'phrase',
         '[]',
-        'Gran - '
+        'Gran - ?'
     ),
     new Orkinism(
-        'funnyface',
+        'funny-face',
         'noun',
-        '[]',
-        'Gran - '
+        '[fuhn-ee feys]',
+        'Gran - ?'
     ),
     new Orkinism(
-        'fatface',
+        'fat-face',
         'noun',
-        '[]',
+        '[fat-feys]',
         'Gran - '
     ),
     new Orkinism(
         'weenie',
         'noun',
-        '[]',
+        '[ween-ee]',
         'Scaredy-cat (do not say to Chloe Orkin).'
     ),
     new Orkinism(
         'dookie',
         'noun',
-        '[]',
+        '[dook-ee]',
         'Anything that sticks out a little; the little bump of the back of Ryan\'s head.'
     ),
     new Orkinism(
         'longnecks',
         'noun',
-        '[]',
+        '[lawng-nek]',
         'Giraffes (not to be confused with baby giraffes).'
     ),
     new Orkinism(
-        '"what\'s that screaming?"',
+        '"What\'s that screaming?"',
         'phrase',
-        '[]',
+        '[hwuhts that skree-ming]',
         'Uttered whenever Hilton is playing his music.'
     ),
     new Orkinism(
         'just quickly',
         'phrase',
-        '[]',
+        '[juhst kwik-lee]',
         'Slowly.'
     ),
     new Orkinism(
         'duck walk',
         'phrase',
-        '[]',
+        '[duhk wawk]',
         'When Tappy Time has concluded and time must be made up.'
     ),
     new Orkinism(
         'little',
         'phrase',
-        '[]',
+        '[lit-l]',
         'Can be used to refer to anything from tiny to huge (example: A tiny coffee could be a quick takeaway or a long sit-down lunch).'
     ),
     new Orkinism(
         'tappy time',
         'phrase',
-        '[]',
+        '[tap-ee tahym]',
         'When someone is late (refer tappy).'
     ),
     new Orkinism(
-        'the red thing',
+        'the red thing; red rocket',
         'phrase',
-        '[]',
+        '[thee red thing]',
         'Tysie\'s doodle (refer doodle).'
     ),
     new Orkinism(
         'mommying',
         'phrase',
-        '[]',
+        '[mom-ee-ing]',
         'Sleeping on your back and not moving - mostly for Hilton.'
     ),
     new Orkinism(
         'na brah',
         'phrase',
-        '[]',
+        '[nah brah]',
         'No.'
     ),
     new Orkinism(
-        'googles; goggaloos',
+        'goggles; goggaloos',
         'phrase',
-        '[]',
+        '[gog-uhls]',
         'Glasses (Chloe).'
     ),
     new Orkinism(
-        '"thanks mum!"',
+        '"Thanks Mum!"',
         'phrase',
-        '[]',
+        '[thangks muhm]',
         'Appreciation for something done by Hilton.'
     ),
 
